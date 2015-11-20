9b6bbc40248e4195672dd77d7422d59f81a12c5d
Evan Tahler <evantahler@gmail.com>
2015-11-19 16:35:32 -0800

    best we can get with proxy

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var fs        = require('fs');
 var glob      = require('glob');
 var http      = require('http');
+var https     = require('https');
 var httpProxy = require('http-proxy');
+
+http.globalAgent.maxSockets  = 1000;
+https.globalAgent.maxSockets = 1000;
 
 var publicDir       = process.env.PUBLIC_DIR   || __dirname + '/public';
 var target          = process.env.PROXY_TARGET || 'http://127.0.0.1:8080';
-var serverPort      = process.env.PORT         || 8008;
+var serverPort      = process.env.PORT         || 8080;
 var proxyHostHeader = process.env.PROXY_HOST   || null;
 
 var publicFiles = [];
@@ -19,7 +23,8 @@ var proxy = httpProxy.createProxyServer({
 });
 
 proxy.on('proxyReq', function(proxyReq, req, res, options){
-  if(proxyHostHeader){ proxyReq.setHeader('HOST', proxyHostHeader); }
+  proxyReq.setHeader('X-proxy-note', 'taskrabbit-sailthru-proxy');
+  if(proxyHostHeader){ proxyReq.setHeader('Host', proxyHostHeader); }
 });
 
 var server = http.createServer(function(req, res){
