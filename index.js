2fc7824420b70e2c85eaefb212a78e5b3b8921c8
Evan Tahler <evantahler@gmail.com>
2015-11-19 21:37:53 -0800

    mime handling and example apple-app-site-association

--- index.js
+++ index.js
@@ -3,21 +3,53 @@ var glob      = require('glob');
 var http      = require('http');
 var https     = require('https');
 var request   = require('request');
+var mime      = require('mime');
 
 http.globalAgent.maxSockets  = 1000;
 https.globalAgent.maxSockets = 1000;
+
+/////////////////////////////
+// CONFIGURATION VARIALBES //
+/////////////////////////////
 
 var publicDir       = process.env.PUBLIC_DIR   || __dirname + '/public';
 var target          = process.env.PROXY_TARGET || 'http://127.0.0.1:8080';
 var serverPort      = process.env.PORT         || 8080;
 var proxyHostHeader = process.env.PROXY_HOST   || null;
+var defaultMime     = 'application/json';
+
+/////////////////////////////
+// STATIC FILE SERVER INIT //
+/////////////////////////////
 
 var publicFiles = [];
 glob.sync(publicDir + '/**/*').forEach(function(f){
   publicFiles.push( f.replace(publicDir,'') );
 });
 
-var doProxy = function(req,res){
+//////////////////////////
+// FILE SERVER HANDLER //
+/////////////////////////
+
+var doStaticFile = function(req, res){
+  var filePath = publicDir + req.url;
+  var fileParts = filePath.split('/');
+  var file = fileParts[ (fileParts.length - 1) ];
+  var extensionParts = file.split('.');
+  if(extensionParts.length > 1){
+    var contentType = mime.lookup(filePath);
+    res.setHeader("Content-Type", contentType);
+  }else{
+    res.setHeader("Content-Type", defaultMime);
+  }
+  fs.createReadStream(filePath).pipe(res);
+};
+
+///////////////////
+// PROXY HANDLER //
+///////////////////
+
+var doProxy = function(req, res){
   var proxyOptions = {
     url: target + req.url,
     followRedirect: false,
@@ -28,13 +60,13 @@ var doProxy = function(req,res){
     proxyOptions.headers[key] = req.headers[key];
   }
 
-  delete proxyOptions.headers.host;
-  delete proxyOptions.headers.Host;
-  delete proxyOptions.headers.HOST;
+  ['host', 'Host', 'HOST'].forEach(function(h){
+    delete proxyOptions.headers[h];
+  });
+  proxyOptions.headers.Host = proxyHostHeader;
 
   proxyOptions.headers['X-Proxy-Note'] = 'taskrabbit-sailthru-proxy';
   proxyOptions.headers['X-Forwarded-For'] = req.connection.remoteAddress;
-  proxyOptions.headers['Host'] = proxyHostHeader;
 
   request(proxyOptions, function(error, response){
     if(error){ console.log(error); }
@@ -47,10 +79,14 @@ var doProxy = function(req,res){
   });
 };
 
+////////////
+// SERVER //
+////////////
+
 var server = http.createServer(function(req, res){
   if(publicFiles.indexOf(req.url) >= 0){
     console.log('[file] ' + req.connection.remoteAddress + ' -> ' + req.url);
-    fs.createReadStream(publicDir + req.url).pipe(res);
+    doStaticFile(req, res);
   }else{
     console.log('[proxy] ' + req.connection.remoteAddress + ' -> ' + req.url);
     doProxy(req, res);    
