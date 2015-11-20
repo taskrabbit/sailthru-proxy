a5b3d888405b8a76c5ac4bd11ff29e57d78ad7f7
Evan Tahler <evantahler@gmail.com>
2015-11-20 12:00:52 -0800

    allow for staing and prod site files

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ var publicDir       = process.env.PUBLIC_DIR   || __dirname + '/public';
 var target          = process.env.PROXY_TARGET || 'http://127.0.0.1:8080';
 var serverPort      = process.env.PORT         || 8080;
 var proxyHostHeader = process.env.PROXY_HOST   || null;
-var defaultMime     = 'application/json';
+
+// This host can return multiple files based on the HOST header of the request
+var siteFileMappings = {
+  '*'                                     : __dirname + '/apple-app-site-association/production.json',
+  'sailthru-links.taskrabbit.net'         : __dirname + '/apple-app-site-association/production.json',
+  'sailthru-proxy.taskrabbit.net'         : __dirname + '/apple-app-site-association/production.json',
+  'staging-sailthru-proxy.taskrabbit.net' : __dirname + '/apple-app-site-association/staging.json',
+};
 
 /////////////////////////////
 // STATIC FILE SERVER INIT //
@@ -27,21 +34,28 @@ glob.sync(publicDir + '/**/*').forEach(function(f){
   publicFiles.push( f.replace(publicDir,'') );
 });
 
+/////////////////////
+// APPLE SITE FILE //
+/////////////////////
+
+var doAppleSiteFile = function(req, res){
+  var filePath = siteFileMappings['*'];
+  if( siteFileMappings[req.headers.host] ){
+    filePath = siteFileMappings[req.headers.host];
+  }
+
+  res.setHeader("Content-Type", 'application/json');
+  fs.createReadStream(filePath).pipe(res);
+};
+
 //////////////////////////
 // FILE SERVER HANDLER //
 /////////////////////////
 
 var doStaticFile = function(req, res){
   var filePath = publicDir + req.url;
-  var fileParts = filePath.split('/');
-  var file = fileParts[ (fileParts.length - 1) ];
-  var extensionParts = file.split('.');
-  if(extensionParts.length > 1){
-    var contentType = mime.lookup(filePath);
-    res.setHeader("Content-Type", contentType);
-  }else{
-    res.setHeader("Content-Type", defaultMime);
-  }
+  var contentType = mime.lookup(filePath);
+  res.setHeader("Content-Type", contentType);
   fs.createReadStream(filePath).pipe(res);
 };
 
@@ -80,7 +94,11 @@ var doProxy = function(req, res){
 ////////////
 
 var server = http.createServer(function(req, res){
-  if(publicFiles.indexOf(req.url) >= 0){
+  if(req.url === '/apple-app-site-association'){
+    console.log('[apple-app-site-association] ' + req.connection.remoteAddress + ' -> ' + req.url);
+    doAppleSiteFile(req, res);
+  }
+  else if(publicFiles.indexOf(req.url) >= 0){
     console.log('[file] ' + req.connection.remoteAddress + ' -> ' + req.url);
     doStaticFile(req, res);
   }else{
