05fc21780ab735430d7bcff642028a68067a9f07
Evan Tahler <evantahler@gmail.com>
2015-11-19 16:11:20 -0800

    support for static files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var fs        = require('fs');
+var glob      = require('glob');
 var http      = require('http');
 var httpProxy = require('http-proxy');
 
+var publicDir       = process.env.PUBLIC_DIR   || __dirname + '/public';
 var target          = process.env.PROXY_TARGET || 'http://127.0.0.1:8080';
 var serverPort      = process.env.PORT         || 8008;
 var proxyHostHeader = process.env.PROXY_HOST   || null;
+
+var publicFiles = [];
+glob.sync(publicDir + '/**/*').forEach(function(f){
+  publicFiles.push( f.replace(publicDir,'') );
+});
 
 var proxy = httpProxy.createProxyServer({
   target: target, // Where do we send all requests?
@@ -15,10 +23,15 @@ proxy.on('proxyReq', function(proxyReq, req, res, options){
 });
 
 var server = http.createServer(function(req, res){
-  console.log('[proxy] request from ' + req.connection.remoteAddress);
-  proxy.web(req, res);
+  if(publicFiles.indexOf(req.url) >= 0){
+    console.log('[file] ' + req.connection.remoteAddress + ' -> ' + req.url);
+    fs.createReadStream(publicDir + req.url).pipe(res);
+    // res.end();
+  }else{
+    console.log('[proxy] ' + req.connection.remoteAddress + ' -> ' + req.url);
+    proxy.web(req, res);
+  }
 });
 
 server.listen(serverPort);
-
-console.log("*** PROXY BOOTED ***");
+console.log('*** PROXY BOOTED @ ' + serverPort + ' ***');
